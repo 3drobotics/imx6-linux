f0af92f27bbda5b0048aaffe869250fe1cf85593
Allan Matthew <allanmatthew@github.com>
2014-07-14 09:34:32 -0700

    New config

---
+++ build3DR.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export LOADADDR=10800000
+
+make imx6solo_defconfig
+make -j24 uImage 
+make -j24 dtbs 
+make -j24 modules
+
+export INSTALL_MOD_PATH=~/Desktop/tmp
+make -j24 modules_install
