35446f8831053d9a24a995c69836f8452f7935ad
Allan Matthew <amatthew@3drobotics.com>
2014-11-13 11:11:38 -0800

    Adding GPIOs for artoo, cleaning up build script

--- build3DR.sh
+++ build3DR.sh
@@ -4,6 +4,9 @@
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 export LOADADDR=10800000
+export INSTALL_MOD_PATH=./mod_install
+rm -rf $INSTALL_MOD_PATH
+mkdir $INSTALL_MOD_PATH
 
 make distclean
 make clean
@@ -11,7 +14,4 @@ make imx6solo_defconfig
 make -j24 uImage 
 make -j24 dtbs 
 make -j24 modules
-
-export INSTALL_MOD_PATH=~/Desktop/tmp
-rm -rf $INSTALL_MOD_PATH/*
-#make -j24 modules_install
+make -j24 modules_install
